a14ac4b334e54bc5259ed33b13bc5c059e80beb2
林本和士 <rihanesu@hayashihonwashinoMacBook-Pro.local>
2020-05-12 11:56:59 +0900

    500

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -174,7 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        
+
         /* 追加 */
         Jenssegers\Agent\AgentServiceProvider::class,
 
